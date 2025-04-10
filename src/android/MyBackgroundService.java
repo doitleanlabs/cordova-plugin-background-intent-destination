49387acef9c3c2fa88392100a832ac2a384bf73b
vsalvalagiodoitlean <victor.salvalagio@doitlean.com>
2025-04-10 10:36:13 +0100

    Update MyBackgroundService.java

--- src/android/MyBackgroundService.java
+++ src/android/MyBackgroundService.java
@@ -13,6 +13,9 @@ public class MyBackgroundService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "Background service started");
 
+        // Required for foreground service
+        Notification notification = createNotification();
+        startForeground(1, notification);
         // You can read extras from the intent if needed
         if (intent != null && intent.hasExtra("exampleData")) {
             String data = intent.getStringExtra("exampleData");
@@ -24,6 +27,7 @@ public class MyBackgroundService extends Service {
         broadcastIntent.putExtra("result", "Here's your data from service!");
         sendBroadcast(broadcastIntent);
 
+        stopSelf();
         return START_NOT_STICKY;
     }
 
